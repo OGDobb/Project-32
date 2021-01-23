c836ee0ae048959c90c23c1bd3917b25e0249ad7
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-01-23 15:27:31 -0800

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,41 @@
+class Block{
+    constructor(x,y,options){
+        var options = {
+            isStatic:false,
+            restitution:0,
+            friction:1,
+            density:1.2
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        //this.image = loadImage("sprites/stone.png");
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        fill("green");
+        //push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        imageMode(CENTER);
+        image(this.image, 0, 0, this.width, this.height);console.log(this.body.speed);
+    
+        if (this.body.speed<3) {
+          super.display();
+        }
+        else{
+          World.remove(world,this.body);
+          push();
+          this.Visibility=this.Visibility-5;
+          tint(255,this.Visibility);
+          image(this.image,this.body.position.x,this.body.position.y,50,50);
+          pop();
+        }
+        //pop();
+      }
+      calculateScore(){
+        if (this.Visibility<0 && this.Visibility>-105){
+          score=++;
+        }
+    }
---
+++ sketch.js
@@ -0,0 +1,74 @@
+
+//const Engine = Matter.Engine;
+//const World = Matter.World;
+//const Bodies = Matter.Bodies;
+//const Body = Matter.Body;
+
+var block1, block2, block3, block4, block5, blcok6, block7, block8, block9, stone, platform;
+var slingshot;
+
+var stoneImg;
+
+function preload() {
+    stoneImg = loadImage("sprite/polygon.png");
+}
+
+function setup() {
+    var canvas = createCanvas(1200,400);
+
+    engine = Engine.create();
+    world = engine.world;
+    
+    Engine.run(engine);
+
+    block1 = new Block(330,235,30,40);
+    block2 = new Block(360,235,30,40);
+    block3 = new Blcok(390,235,30,40);
+    block4 = new Block(420,235,30,40);
+    block5 = new Block(450,235,30,40);
+
+    block6 = new Block(360,195,30,40);
+    block7 = new Block(390,195,30,40);
+    block8 = new Block(420,195,30,40);
+
+    block9 = new Block(390,155,30,40);
+
+    platform = new Platform(390,200,200,10);
+
+    slingshot = new SlingShot(50,200,10,10);
+
+    stone = new Stone(50,200,10,10)
+}
+
+function draw() {
+    rectMode(CENTER);
+    background(0);
+    block1.display();
+    block2.display();
+    block3.display();
+    block4.display();
+    block5.display();
+    block6.display();
+    block7.display();
+    block8.display();
+    block9.display();
+    platform.display();
+    slingshot.display();
+    stone.display();
+
+    text("score:"+score,1000,50);
+
+}
+
+function mouseDragged(){
+	Matter.Body.setPosition(stone.body,{x:mouseX,y:mouseY});
+	}
+	
+	function mouseReleased(){
+    slingshot.fly();
+    
+    function keyPressed() {
+        if (keyCode===32) {
+            slingshot.attach(bird.body);
+        }
+    }
